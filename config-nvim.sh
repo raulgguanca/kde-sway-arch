da52995dbc9fabea23206fa9c19d87e363ea0b24
raulgguanca <raulgguanca@gmail.com>
2026-01-07 12:48:33 -0300

    feat: add output comments to the script sequence
    - improved visual sequence for the scripts with comments

--- config-nvim.sh
+++ config-nvim.sh
@@ -1,15 +1,21 @@
 #! bin/bash
 set -e
 
+echo -e 'creating directories\n'
+
 mkdir -p ~/.vim/plugged
 
 sudo mkdir -p /root/.config/nvim
 
 sudo mkdir -p /root/.vim/plugged
 
+echo -e 'copying files to root\n'
+
 sudo cp ~/.config/nvim/init.vim /root/.config/nvim/
 
 sudo cp ~/.config/nvim/coc-settings.json /root/.config/nvim/
+
+echo -e 'setting up vim-plug\n'
 
 #https://github.com/junegunn/vim-plug
 
@@ -18,6 +24,8 @@ sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo 'finishing up and cleaning'
 
 rm -r config-nvim.sh
 
--- themes.sh
+++ themes.sh
@@ -1,15 +1,39 @@
 #! /bin/bash
 set -e
 
+echo -e 'cloning git Orchis theme\n'
+
 git clone https://github.com/vinceliuice/Orchis-theme
+
+echo -e 'changing to git directory\n'
+
 cd Orchis-theme
+
+echo -e 'installing Orchis red compact theme\n'
+
 ./install.sh -t red --tweaks compact
+
+echo -e 'cleaning\n'
+
 cd .. && rm -rf Orchis-theme
 
+echo -e 'cloning git Tela Circle theme\n'
+
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
+
+echo -e 'changing to git directory\n'
+
 cd Tela-circle-icon-theme
+
+echo -e 'installing Tela Circle red theme\n'
+
 ./install.sh -c red 
+
+echo -e 'cleaning\n'
+
 cd .. && rm -rf Tela-circle-icon-theme
+
+echo 'finishing up and cleaning'
 
 rm -rf themes.sh
 
